6bf60dcd88f1ef7b8da314df4386beafa359f592
Thiago William <thiagokepler1@gmail.com>
2023-09-07 19:38:17 -0300

    ref/separa front e back em pastas

--- src/api/server.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-require("express-async-errors");
-
-const port = process.env.PORT || 3001;
-const app = require("./app");
-
-app.listen(port);
-console.log(`Executando API na porta ${port}`);
